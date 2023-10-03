95b5baef3da27da99e9a907b7cab7db7c98a0f68
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-10-02 23:25:37 -0300

    Instalacion de react-bootstrap. Nuevos componetes y archivos de estilos. Modelo de navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './Style/App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Style/App.css";
+import Rutas from "./Components/Routes/Rutas";
+import Header from "./Components/Routes/Header";
 
 const App = () => {
-  
   return (
     <>
-      
+      <Header />
+      <Rutas />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../Style/Home.css";
+
+
+const Home = () => {
+  return (
+    <>
+     
+    </>
+  );
+};
+
+export default Home;
